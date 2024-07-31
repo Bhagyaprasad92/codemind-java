ec3ef6d4b2698ca4f5f89ae067b412dc5d6803db
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2024-07-31 16:03:06 +0530

    import java.util.Scanner; public class Example { public static void main(String[] args){ int a,b,c; Scanner read = new Scanner(System.in); a = read.nextInt(); b = read.nextInt(); System.out.println(a*b); } }

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Example {
+public static void main(String[] args){
+int a,b,c;
+Scanner read = new Scanner(System.in);
+a = read.nextInt();
+b = read.nextInt();
+System.out.println(a*b);
+}
+}
